e4233e4fac0da2c3bb90db45eb7d00d65ae2352f
Anto7304 <antonywanyaga2004@gmail.com>
2026-05-13 01:39:48 +0300

    navbar fixed for index

--- index.php
+++ index.php
@@ -16,10 +16,12 @@
                 <h2>🏠 RealEstate</h2>
             </div>
             <ul class="nav-links">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#properties">Properties</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li><a href="/">Home</a></li>
+            <li><a href="/properties.php">Properties</a></li>
+            <li><a href="/about.php">About</a></li>
+            <li><a href="/contact.php">Contact</a></li>
+            <li><a href="/frontend/login.php">Login</a></li>
+            <li><a href="/frontend/register.php">Register</a></li>
             </ul>
             <div class="nav-buttons" id="navButtons">
                 <a href="login.php" class="btn-login">Login</a>
