f58d5022508b73d0502b3c512f58e2fa73a331a1
Anto7304 <antonywanyaga2004@gmail.com>
2026-05-13 12:27:42 +0300

    cleaned

--- test-clean.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "<h1>Clean Test - Server Working!</h1>";
-echo "<p>PHP Version: " . phpversion() . "</p>";
-?>
--- test.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "<h1>Server Working!</h1>";
-echo "<p>PHP Version: " . phpversion() . "</p>";
-?>
